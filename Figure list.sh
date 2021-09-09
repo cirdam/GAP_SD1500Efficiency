662c2d57f159a0a838798a4c6e7b4356ace3c85a
cirdam <pgbrichetto@gmail.com>
2021-09-09 20:52:25 -0300

    Update 9/9

---
+++ Figure list.sh
@@ -0,0 +1,59 @@
+#Figure list:
+
+#Energy 
+
+cp /home/glorfindel/Sd1500EfficiencyUpdate/updateDataset/OldTriggers/plots/energy45.pdf .
+
+#Efficiency 0 to 60
+
+cp /home/glorfindel/Sd1500EfficiencyUpdate/updateDataset/OldTriggers/plots/allZenith.pdf . 
+
+#Efficiency Zenith 1 bin
+
+cp /home/glorfindel/Sd1500EfficiencyUpdate/updateDataset/OldTriggers/plots/singleZenithFit.pdf .
+
+#Zenith Parameters
+
+cp /home/glorfindel/Sd1500EfficiencyUpdate/updateDataset/OldTriggers/plots/E0.pdf .
+
+cp /home/glorfindel/Sd1500EfficiencyUpdate/updateDataset/OldTriggers/plots/b.pdf .
+
+
+#Efficiency Zenith multiple bins
+
+cp /home/glorfindel/Sd1500EfficiencyUpdate/updateDataset/OldTriggers/plots/EfficiencyZenith.pdf .
+
+#Efficiency Surface
+
+cp /home/glorfindel/Sd1500EfficiencyUpdate/updateDataset/OldTriggers/plots/Surface.pdf .
+
+#New Triggers
+
+cp /home/glorfindel/Sd1500EfficiencyUpdate/updateDataset/NewTriggers/plots/NewCut.pdf .
+
+#Spectrum
+
+cp /home/glorfindel/Sd1500EfficiencyUpdate/spec/plots/spectrum.pdf .
+
+#Spectrum difference
+
+cp /home/glorfindel/Sd1500EfficiencyUpdate/spec/plots/NewDifference.pdf .
+
+#New Triggers Parameters
+
+cp /home/glorfindel/Sd1500EfficiencyUpdate/updateDataset/NewTriggers/plots/E0New.pdf .
+
+cp /home/glorfindel/Sd1500EfficiencyUpdate/updateDataset/NewTriggers/plots/bNew.pdf .
+
+#New Trigger Surface
+
+cp /home/glorfindel/Sd1500EfficiencyUpdate/updateDataset/NewTriggers/plots/SurfaceNew.pdf .
+
+cp /home/glorfindel/Sd1500EfficiencyUpdate/updateDataset/NewTriggers/plots/EfficiencyZenithNew.pdf .
+
+
+
+
+
+
+
